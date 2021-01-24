807814a09b618b4e054bd717d23a12d762e8169a
marunouchiR5 <53039597+marunouchiR5@users.noreply.github.com>
2021-01-24 19:03:57 +0800

    Add files via upload

---
+++ example12/py_lex.py
@@ -0,0 +1,60 @@
+#! /usr/bin/env python
+# coding=utf-8
+import ply.lex as lex
+
+# LEX for parsing Python
+
+# Tokens
+tokens = ('VARIABLE', 'NUMBER', 'PRINT', 'DEF', 'CLASS', 'SELF', 'STR')
+
+literals = ['=', '+', '-', '*', '(', ')', '{', '}', '<', '>', ',', '.']
+
+
+# Define of tokens
+# 加入字符串的分析
+
+def t_NUMBER(t):
+    r'[0-9]+'
+    return t
+
+
+def t_STR(t):
+    r'\'\w*\''
+    return t
+
+
+def t_PRINT(t):
+    r'(?<!\.)print(?=\()'
+    return t
+
+
+def t_DEF(t):
+    r'def'
+    return t
+
+
+def t_CLASS(t):
+    r'class'
+    return t
+
+
+def t_SELF(t):
+    r'self'
+    return t
+
+
+def t_VARIABLE(t):
+    r'[a-zA-Z\$_][a-zA-Z\d_]*'
+    return t
+
+
+# Ignored
+t_ignore = " \t"
+
+
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+
+lex.lex()
